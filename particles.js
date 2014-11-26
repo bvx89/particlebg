1893db4176fbe9d320b893fc2fe61aea11fde2eb
Lasse Brudeskar Vikås <bvx89.at.cs@gmail.com>
2014-11-26 14:49:47 +0100

    Update for performance

--- particles.js
+++ particles.js
@@ -1,7 +1,6 @@
 var Particles = (function () {
 	// DOM
 	var ctx,
-		background,
 	
 	// Engine
 		meter,
@@ -11,10 +10,10 @@ var Particles = (function () {
 		step = 1/60,
 	
 	// Properties
-		maxSpeed = 7,
-		maxAmount = 60,
-		maxSize = 1.5,
-		randomization = 0.1;
+		maxSpeed = 50,
+		maxAmount = 50,
+		maxSize = 1,
+		randomization = 20;
 		width = window.innerWidth,
 		height = window.innerHeight;
 
@@ -22,9 +21,7 @@ var Particles = (function () {
 		// Set up canvas context
 		ctx = document.getCSSCanvasContext('2d', 'background', 
 											   width, height);		
-											   
-		background = getBackgroundGradient();
-											   
+											  											   
 		// Generate all initial particles
 		particles = genParticles(maxAmount, width, height);
 
@@ -47,7 +44,6 @@ var Particles = (function () {
 
 		render(dt);
 		last = now;
-		
 		requestAnimFrame(animate);
 	},
 	
@@ -56,26 +52,44 @@ var Particles = (function () {
 		for (var i = particles.length - 1; i >= 0; i--) {
 			p = particles[i];
 			
+			// Add randomness
+			if (Math.random() < 0.01) {
+				var vx = addRandom(randomization),
+					vy = addRandom(randomization);
+				
+				if (p.vx + vx > maxSpeed) {
+					p.vx = maxSpeed;
+				} else if (p.vx < -maxSpeed) {
+					p.vx = -maxSpeed;
+				} else {
+					p.vx += vx;
+				}				
+					
+				if (p.vy + vy > maxSpeed) {
+					p.vy = maxSpeed;
+				} else if (p.vy < -maxSpeed) {
+					p.vy = -maxSpeed;
+				} else {
+					p.vy += vy;
+				}
+			}
+			
+			// Add speed
+			p.x += p.vx * dt;
+			p.y += p.vy * dt;
+			
 			// Opposite direction if out of bounds
 			if (p.x + p.r > width || p.x - p.r < 0) {
 				p.vx *= -1;
 			} else if (p.y + p.r > height || p.y - p.r < 0) {
 				p.vy *= -1;
 			}
-			
-			// Add speed
-			p.x += p.vx * dt + addRandom(randomization);
-			p.y += p.vy * dt + addRandom(randomization);
 		}
 	},
 	
 	render = function(dt) {
 		// Draw BG
-		ctx.save();
-		ctx.fillStyle = background;
-		ctx.fillRect(0,0,width,height);		
-		ctx.restore();
-		
+		ctx.clearRect(0,0,width, height);
 		
 		// Draw particles
 		ctx.fillStyle = '#FCF6CF';
@@ -96,8 +110,8 @@ var Particles = (function () {
 			var p = {
 					x : Math.random() * width,
 					y : Math.random() * height,
-					vx : maxSpeed * addRandom(maxSpeed),
-					vy : maxSpeed * addRandom(maxSpeed),
+					vx : addRandom(maxSpeed),
+					vy : addRandom(maxSpeed),
 					r : Math.random() * maxSize + 1
 					};
 			arr.push(p);
@@ -108,31 +122,14 @@ var Particles = (function () {
 	
 	addRandom = function(val) {
 		return (Math.random() * val - (val/2));
-	},
-	
-	getBackgroundGradient = function() {
-		// Create gradient
-		var grd = ctx.createLinearGradient(0, 0, 0, height);
-
-		// Add colors
-		grd.addColorStop(0.000, 'rgba(234, 100, 100, 1.000)');
-		grd.addColorStop(0.500, 'rgba(237, 111, 111, 1.000)');
-		grd.addColorStop(1.000, 'rgba(244, 39, 39, 1.000)');
-
-		// Fill with gradient
-		return grd;
-	};
+	}
 
 	return {
 		begin : function() {
 			init();
 			requestAnimFrame(animate);
 			
-			window.onresize = function(event) {
-				width = window.innerWidth,
-				height = window.innerHeight;
-				background = getBackgroundGradient();
-			}
+			
 		}
 	}
 })();
